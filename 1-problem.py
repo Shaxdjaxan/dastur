6517f2eb00c28e69f0e61f10e4a01de76b249675
Shohjahon Nomozov <shaxdjaxan98@gmail.com>
2024-01-26 10:52:42 +0500

    birinchi versiya

---
+++ 1-problem.py
@@ -0,0 +1,11 @@
+# def how(st):
+#     print(set(st[:-1]))
+# how(input("Enter a string: "))
+def function(lst: list):
+    newlist = list.split(" ")
+
+    for i in newlist:
+        if i[0]=='K' or i[0]=='k':
+            print(i)
+
+function(list(input("soz kiriting")))
---
+++ text.txt
@@ -0,0 +1,11 @@
+def funk(a, natija):
+    natija+=a
+    return natija
+son=[]
+i=0
+natija = 0
+while i != 'c':
+    i=(int(input("son kiriting")), natija)
+    if i=='c':
+        break
+print(natija)
