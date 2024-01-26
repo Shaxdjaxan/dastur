18e6b31d148c7986ae39c49c11245e6cd58fa728
Shohjahon Nomozov <shaxdjaxan98@gmail.com>
2024-01-26 10:57:48 +0500

    ikkinchi versiya

--- 1-problem.py
+++ 1-problem.py
@@ -9,3 +9,28 @@ def function(lst: list):
             print(i)
 
 function(list(input("soz kiriting")))
+
+def function(lst: list):
+    newlist = list.split(" ")
+
+    for i in newlist:
+        if i[0]=='K' or i[0]=='k':
+            print(i)
+
+function(list(input("soz kiriting")))
+def function(lst: list):
+    newlist = list.split(" ")
+
+    for i in newlist:
+        if i[0]=='K' or i[0]=='k':
+            print(i)
+
+function(list(input("soz kiriting")))
+def function(lst: list):
+    newlist = list.split(" ")
+
+    for i in newlist:
+        if i[0]=='K' or i[0]=='k':
+            print(i)
+
+function(list(input("soz kiriting")))
